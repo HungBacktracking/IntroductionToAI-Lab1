eaa773cb552d480d289919ad59902874c57f51aa
Võ Trung Hoàng Hưng <21120011@student.hcmus.edu.vn>
2023-10-25 20:54:51 +0700

    python3 -> python

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
-python3 ./source/main.py --input=level_1/input1.txt --output=level_1/input1
-python3 ./source/main.py --input=level_1/input2.txt --output=level_1/input2
-python3 ./source/main.py --input=level_1/input3.txt --output=level_1/input3
-python3 ./source/main.py --input=level_1/input4.txt --output=level_1/input4
-python3 ./source/main.py --input=level_1/input5.txt --output=level_1/input5
+python ./source/main.py --input=level_1/input1.txt --output=level_1/input1
+python ./source/main.py --input=level_1/input2.txt --output=level_1/input2
+python ./source/main.py --input=level_1/input3.txt --output=level_1/input3
+python ./source/main.py --input=level_1/input4.txt --output=level_1/input4
+python ./source/main.py --input=level_1/input5.txt --output=level_1/input5
 
-python3 ./source/main.py --input=level_2/input1.txt --output=level_2/input1
-python3 ./source/main.py --input=level_2/input2.txt --output=level_2/input2
-python3 ./source/main.py --input=level_2/input3.txt --output=level_2/input3
+python ./source/main.py --input=level_2/input1.txt --output=level_2/input1
+python ./source/main.py --input=level_2/input2.txt --output=level_2/input2
+python ./source/main.py --input=level_2/input3.txt --output=level_2/input3

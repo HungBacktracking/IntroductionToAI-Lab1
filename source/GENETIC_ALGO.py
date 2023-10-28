e01fa5d81116ca302fc54e1202105930a3368d79
Võ Trung Hoàng Hưng <21120011@student.hcmus.edu.vn>
2023-10-28 15:09:44 +0700

    add path from start and end

--- source/GENETIC_ALGO.py
+++ source/GENETIC_ALGO.py
@@ -12,10 +12,38 @@ def randomSolution(tsp):
 
     return solution
 
-def setupCostCities(cities, matrix):
+def setupCostCities(cities, start, end, matrix):
     distance = {}
+    distance[start] = {}
+    distance[end] = {}
     for city in cities:
         distance[city] = {}
+
+        first = start
+        second = city
+        route, explored, cost = BFS_Tele(matrix, first, second, [])
+
+        distance[first][second] = {}
+        distance[second][first] = {}
+        distance[first][second]['route'] = route
+        distance[first][second]['explored'] = explored
+        distance[first][second]['cost'] = cost
+        distance[second][first]['route'] = route
+        distance[second][first]['explored'] = explored
+        distance[second][first]['cost'] = cost
+
+        first = end
+        route, explored, cost = BFS_Tele(matrix, first, second, [])
+
+        distance[first][second] = {}
+        distance[second][first] = {}
+        distance[first][second]['route'] = route
+        distance[first][second]['explored'] = explored
+        distance[first][second]['cost'] = cost
+        distance[second][first]['route'] = route
+        distance[second][first]['explored'] = explored
+        distance[second][first]['cost'] = cost
+
 
     for i in range(len(cities) - 1):
         for j in range(len(cities)):
@@ -39,5 +67,6 @@ def GENETIC_ALGO(matrix, start, end, bonus):
     for val in bonus:
         cities.append((val[0], val[1]))
     
-    distance = setupCostCities(cities, matrix)
+    distance = setupCostCities(cities, start, end, matrix)
+    
     print(distance)

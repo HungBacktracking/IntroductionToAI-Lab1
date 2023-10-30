0717830337938ff3f296b0be33982324928737f3
Võ Trung Hoàng Hưng <21120011@student.hcmus.edu.vn>
2023-10-30 17:19:12 +0700

    finish dijkstra for lv2

---
+++ source/DIJKSTRA.py
@@ -0,0 +1,89 @@
+from queue import PriorityQueue
+
+def isPointValid(matrix, point):
+    if (point[0] < len(matrix) and point[0] >= 0 and point[1] < len(matrix[0]) and point[1] >= 0 and matrix[point[0]][point[1]] != 'x'):
+        return True
+    return False
+
+class Node:
+    def __init__(self, position, _weight, _bitmask):
+        self.position = position
+        self.weight = _weight
+        self.bitmask = _bitmask
+    
+    def __lt__(self, other):
+        return self.weight < other.weight
+    
+def get_weight(point, bonus_points):
+    for i in range(len(bonus_points)):
+        val = bonus_points[i]
+        if point == (val[0], val[1]):
+            return val[2] + 1, i
+    return 1, -1
+
+def Dijkstra(matrix, start, end, bonus_points):
+    INF = 10 ** 10
+    mask_size = (1 << 10)
+
+    frontier = PriorityQueue()
+    frontier.put(Node(start, 0, 0))
+
+    cost = [[[INF for i in range(mask_size)] for j in range(50)] for k in range(50)]
+    trace = [[[None for i in range(mask_size)] for j in range(50)] for k in range(50)]
+    cost[start[0]][start[1]][0] = 0
+
+    visited = []
+    dicrections = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+    while not frontier.empty():
+        current = frontier.get()
+        cur_position = current.position
+        cur_weight = current.weight
+        cur_bitmask = current.bitmask
+
+        if cur_position not in visited:
+            visited.append(cur_position)
+
+        if cur_weight != cost[cur_position[0]][cur_position[1]][cur_bitmask]: 
+            continue
+        for dir in dicrections:
+            point = (cur_position[0] + dir[0], cur_position[1] + dir[1])
+            if isPointValid(matrix = matrix, point = point) != True:
+                continue
+
+            next_weight, id_bonus = get_weight(point, bonus_points)
+            if next_weight < 0:
+                if (cur_bitmask & (1 << id_bonus)) != 0: # Đã đi qua điểm thưởng này rồi, không đi nữa
+                    continue
+                new_bitmask = (cur_bitmask | (1 << id_bonus))
+                if cur_weight + next_weight < cost[point[0]][point[1]][new_bitmask]:
+                    trace[point[0]][point[1]][new_bitmask] = cur_position
+                    cost[point[0]][point[1]][new_bitmask] = cur_weight + next_weight
+                    frontier.put(Node(point, cur_weight + next_weight, new_bitmask))
+            else:
+                if cur_weight + next_weight < cost[point[0]][point[1]][cur_bitmask]:
+                    trace[point[0]][point[1]][cur_bitmask] = cur_position
+                    cost[point[0]][point[1]][cur_bitmask] = cur_weight + next_weight
+                    frontier.put(Node(point, cur_weight + next_weight, cur_bitmask))
+
+    best_cost = INF
+    best_bitmask = -1
+    for bitmask in range(0, (1 << 10)):
+        if best_cost > cost[end[0]][end[1]][bitmask]:
+            best_cost = cost[end[0]][end[1]][bitmask]
+            best_bitmask = bitmask
+
+    route = []
+    check_pos = end
+    check_bitmask = best_bitmask
+    while check_pos != start:
+        print(check_pos)
+        route.append(check_pos)
+        weight, id = get_weight(check_pos, bonus_points)
+        check_pos = trace[check_pos[0]][check_pos[1]][check_bitmask]
+        if id != -1:
+            check_bitmask = (check_bitmask ^ (1 << id))
+
+    route.append(start)
+    route.reverse()
+    print("HOOOOO")
+    return route, visited, best_cost
--- source/main.py
+++ source/main.py
@@ -10,6 +10,7 @@ from HILL_CLIMBING import *
 from GENETIC import *
 from A_Star_lv2 import *
 from DP import *
+from DIJKSTRA import *
 
 def main(argv):
     in_file, out_file = './input/', ''
@@ -40,56 +41,63 @@ def main(argv):
         name = 'BFS'
         route,explored,cost = BFS(matrix,start,end,bonus_points)
         write_cost_path(cost, './output/' + out_file + '/bfs/BFS.txt')
-        visualize_maze(matrix,bonus_points,start,end,out_put,name,route,explored)
+        visualize_maze(matrix,bonus_points,start,end,out_put,name,route,explored,False)
 
         out_put = './output/' + out_file + '/dfs/DFS.jpg'
         name = 'DFS'
         route,explored,cost = DFS_main(matrix,start,end,bonus_points)
         write_cost_path(cost, './output/' + out_file + '/dfs/DFS.txt')
-        visualize_maze(matrix,bonus_points,start,end,out_put,name,route,explored)
+        visualize_maze(matrix,bonus_points,start,end,out_put,name,route,explored,False)
 
         out_put = './output/' + out_file + '/ucs/UCS.jpg'
         name = 'UCS'
         route,explored,cost = UCS(matrix,start,end,bonus_points)
         write_cost_path(cost, './output/' + out_file + '/ucs/UCS.txt')
-        visualize_maze(matrix,bonus_points,start,end,out_put,name,route,explored)
+        visualize_maze(matrix,bonus_points,start,end,out_put,name,route,explored,False)
 
         out_put = './output/' + out_file + '/gbfs/GBFS.jpg'
         name = 'GBFS'
         route,explored,cost = GBFS(matrix,start,end,bonus_points)
         write_cost_path(cost, './output/' + out_file + '/gbfs/GBFS.txt')
-        visualize_maze(matrix,bonus_points,start,end,out_put,name,route,explored)
+        visualize_maze(matrix,bonus_points,start,end,out_put,name,route,explored,False)
 
         out_put = './output/' + out_file + '/astar/astar.jpg'
         name = 'AStar'
         route,explored,cost = AStar(matrix,start,end,bonus_points)
         write_cost_path(cost, './output/' + out_file + '/astar/astar.txt')
-        visualize_maze(matrix,bonus_points,start,end,out_put,name,route,explored)
+        visualize_maze(matrix,bonus_points,start,end,out_put,name,route,explored,False)
 
     if 'level_2' in in_file:
         out_put = './output/' + out_file + '/dp/dp.jpg'
         name = 'DP'
         route,explored,cost = DP(matrix,start,end,bonus_points)
         write_cost_path(cost, './output/' + out_file + '/dp/dp.txt')
-        visualize_maze(matrix,bonus_points,start,end,out_put,name,route,explored)
+        visualize_maze(matrix,bonus_points,start,end,out_put,name,route,explored,False)
+
+        out_put = './output/' + out_file + '/dijkstra/dijkstra.jpg'
+        name = 'Dijkstra'
+        route,explored,cost = Dijkstra(matrix,start,end,bonus_points)
+        write_cost_path(cost, './output/' + out_file + '/dijkstra/dijkstra.txt')
+        visualize_maze(matrix,bonus_points,start,end,out_put,name,route,explored,False)
 
         out_put = './output/' + out_file + '/astar_lv2/astar_lv2.jpg'
         name = 'AStar_LV2'
         route,explored,cost = AStar_Lv2(matrix,start,end,bonus_points)
         write_cost_path(cost, './output/' + out_file + '/astar_lv2/astar_lv2.txt')
-        visualize_maze(matrix,bonus_points,start,end,out_put,name,route,explored)
+        visualize_maze(matrix,bonus_points,start,end,out_put,name,route,explored,False)
+
     if 'level_3' in in_file:
         out_put = './output/' + out_file + '/hill_climbing/hill_climbing.jpg'
         name = 'Hill Climbing'
         route,explored,cost = HILL_CLIMBING(matrix,start,end,bonus_points)
         write_cost_path(cost, './output/' + out_file + '/hill_climbing/hill_climbing.txt')
-        visualize_maze(matrix,bonus_points,start,end,out_put,name,route,explored)
+        visualize_maze(matrix,bonus_points,start,end,out_put,name,route,explored,False)
 
         out_put = './output/' + out_file + '/genetic/genetic.jpg'
         name = 'Genetic Algorithm'
         route,explored,cost = GENETIC(matrix,start,end,bonus_points)
         write_cost_path(cost, './output/' + out_file + '/genetic/genetic.txt')
-        visualize_maze(matrix,bonus_points,start,end,out_put,name,route,explored)
+        visualize_maze(matrix,bonus_points,start,end,out_put,name,route,explored,False)
 
 
 if __name__ == '__main__':

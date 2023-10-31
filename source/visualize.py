d8c0b040ab26826a53d9693d99bd67081203c5af
glucose20 <21120146@student.hcmus.edu.vn>
2023-10-31 13:45:42 +0700

    zizeo

--- source/visualize.py
+++ source/visualize.py
@@ -1,4 +1,7 @@
 import pygame
+import imageio
+import numpy as np
+from pygame.locals import QUIT
 import sys,os
 import processIO as IO
 import implement as imp
@@ -23,33 +26,50 @@ route=[]
 start=(0,0)
 end=(0,0)
 ALGNAME=''
-TILE = 100
+scale = 100
 FPS = 60
 CELL_WIDTH = 30
 CELL_HEIGHT = 30
+SCREEN_WIDTH = 1280  # 16:9 aspect ratio
+SCREEN_HEIGHT = 720
+SCREEN_SIZE = [SCREEN_WIDTH, SCREEN_HEIGHT]
+map_width = scale * len(matrix[0]) if matrix else 0
+map_height = scale * len(matrix)
+map_x = (SCREEN_WIDTH - map_width) // 2
+map_y = (SCREEN_HEIGHT - map_height) // 2
 X_OFFSET = 10
 Y_OFFSET = 10
 delay = 100
-WALL_IMG = pygame.image.load('./assets/fence.png')
-START_IMG = pygame.image.load('./assets/star.png')
-EXIT_IMG = pygame.image.load('./assets/house.png')
-GIFT_IMG = pygame.image.load('./assets/tree.png')
-TIM1_IMG = pygame.image.load('./assets/paw.png')
-TIM2_IMG = pygame.image.load('./assets/cat.png')
-CLOSE_IMG = pygame.image.load('./assets/close.png')
-OPEN_IMG = pygame.image.load('./assets/open.png')
-TELEPORT = pygame.image.load('./assets/tele.png')
-TELEPORT_OUT = pygame.image.load('./assets/teleout.png')
+WALL_IMG1 = pygame.image.load('./assets/fence.png')
+START_IMG1 = pygame.image.load('./assets/star.png')
+EXIT_IMG1 = pygame.image.load('./assets/house.png')
+GIFT_IMG1 = pygame.image.load('./assets/tree.png')
+TIM1_IMG1 = pygame.image.load('./assets/paw.png')
+TIM2_IMG1 = pygame.image.load('./assets/cat.png')
+CLOSE_IMG1 = pygame.image.load('./assets/close.png')
+OPEN_IMG1 = pygame.image.load('./assets/open.png')
+TELEPORT1 = pygame.image.load('./assets/tele.png')
+TELEPORT_OUT1 = pygame.image.load('./assets/teleout.png')
 
 def draw_cell_no_delay(x, y, IMG, WIN):
-    drawX = X_OFFSET + y * TILE
-    drawY = Y_OFFSET + x * TILE
+    drawX = map_x + y * scale
+    drawY = map_y + x * scale
     WIN.blit(IMG, (drawX, drawY))
 
-def matrix_initialize(matrix):
+def matrix_initialize(matrix,scale):
     cnt =0
     plus=[]
     bns=[]
+    WALL_IMG = pygame.transform.scale(WALL_IMG1, (scale, scale))
+    START_IMG = pygame.transform.scale(START_IMG1, (scale, scale))
+    EXIT_IMG = pygame.transform.scale(EXIT_IMG1, (scale, scale))
+    GIFT_IMG = pygame.transform.scale(GIFT_IMG1, (scale, scale))
+    TIM1_IMG = pygame.transform.scale(TIM1_IMG1, (scale, scale))
+    TIM2_IMG = pygame.transform.scale(TIM2_IMG1, (scale, scale))
+    CLOSE_IMG = pygame.transform.scale(CLOSE_IMG1, (scale, scale))
+    OPEN_IMG = pygame.transform.scale(OPEN_IMG1, (scale, scale))
+    TELEPORT = pygame.transform.scale(TELEPORT1, (scale, scale))
+    TELEPORT_OUT = pygame.transform.scale(TELEPORT_OUT1, (scale, scale))
     for row in range(len(matrix)):
         for col in range(len(matrix[row])):
             if matrix[row][col] == 'x':
@@ -58,7 +78,7 @@ def matrix_initialize(matrix):
             if matrix[row][col] == 'v':
                 draw_cell_no_delay(row, col, TIM1_IMG, screen)
             # if matrix[row][col] == ' ':
-                # pygame.draw.rect(screen, WHITE, (col * TILE, row * TILE, TILE, TILE))
+                # pygame.draw.rect(screen, WHITE, (col * scale, row * scale, scale, scale))
             if (row,col) == start:
                 draw_cell_no_delay(row, col, START_IMG, screen)
             if (row, col) == end:
@@ -72,28 +92,40 @@ def matrix_initialize(matrix):
                 
             if matrix[row][col] == 'b':
                 plus.append((row,col))
-                # pygame.draw.rect(screen, GOODBLUE, (col * TILE, row * TILE, TILE, TILE))
+                # pygame.draw.rect(screen, GOODBLUE, (col * scale, row * scale, scale, scale))
             # if matrix[row][col] == 'O' or matrix[row][col] == 'o':
             #     plus.append((row,col))
-            #     pygame.draw.rect(screen, GOODBLUE, (col * TILE, row * TILE, TILE, TILE))
+            #     pygame.draw.rect(screen, GOODBLUE, (col * scale, row * scale, scale, scale))
     for cell in bns:
         draw_cell_no_delay(cell[0], cell[1], OPEN_IMG, screen)      
     for cell in plus:
         draw_cell_no_delay(cell[0], cell[1], CLOSE_IMG, screen)
 
 def draw_map():
-    matrix_initialize(matrix)
+    matrix_initialize(matrix,scale)
 
 
 def path_finding(dir,alg):
-    global matrix ,BONUS,explored,route,start,end,ALGNAME,screen,TILE
+    global matrix ,BONUS,explored,route,start,end,ALGNAME,screen,scale
     BONUS,matrix, is_teleport= read_file(dir)
-    if len(matrix) >= 20 or len(matrix[0]) >= 20:
-        TILE = 30
+    print("matrix size: ",len(matrix),len(matrix[0]))
+    if len(matrix) >= 15 or len(matrix[0]) >= 35:
+        scale = 20
+        
     else: 
-        TILE = 50
-    WIDTH = TILE*len(matrix[0])  # screen width
-    HEIGHT = TILE*len(matrix)  # screen height
+        scale = 30
+    WALL_IMG = pygame.transform.scale(WALL_IMG1, (scale, scale))
+    START_IMG = pygame.transform.scale(START_IMG1, (scale, scale))
+    EXIT_IMG = pygame.transform.scale(EXIT_IMG1, (scale, scale))
+    GIFT_IMG = pygame.transform.scale(GIFT_IMG1, (scale, scale))
+    TIM1_IMG = pygame.transform.scale(TIM1_IMG1, (scale, scale))
+    TIM2_IMG = pygame.transform.scale(TIM2_IMG1, (scale, scale))
+    CLOSE_IMG = pygame.transform.scale(CLOSE_IMG1, (scale, scale))
+    OPEN_IMG = pygame.transform.scale(OPEN_IMG1, (scale, scale))
+    TELEPORT = pygame.transform.scale(TELEPORT1, (scale, scale))
+    TELEPORT_OUT = pygame.transform.scale(TELEPORT_OUT1, (scale, scale))
+    WIDTH = scale*len(matrix[0])  # screen width
+    HEIGHT = scale*len(matrix)  # screen height
     SCREEN_SIZE = [WIDTH, HEIGHT]
     # B= list(BONUS.keys())
     B = [(point[0], point[1]) for point in BONUS]
@@ -109,22 +141,32 @@ def path_finding(dir,alg):
         route.pop(0)
     else:
         print("No route found!")
-    TILE=int(HEIGHT/len(matrix))
+    scale=int(HEIGHT/len(matrix))
     ALGNAME=alg.__name__.upper()
     pygame.init()
     display_info = pygame.display.Info()
     screen_width = display_info.current_w
     screen_height = display_info.current_h - 50
-    WIDTH = screen_width #screen width
-    HEIGHT = screen_height #screen height
-    SCREEN_SIZE = [WIDTH, HEIGHT]
+    SCREEN_WIDTH = 1280  # 16:9 aspect ratio
+    SCREEN_HEIGHT = 720
+    SCREEN_SIZE = [SCREEN_WIDTH, SCREEN_HEIGHT]
+    screen = pygame.display.set_mode(SCREEN_SIZE)
     screen = pygame.display.set_mode(SCREEN_SIZE) #set screen size
     pygame.display.set_caption("exploredUALIZATION {}".format(ALGNAME)) #set caption
 
     screen.fill((255,255,255))
+    output_file = "output_video.mp4"
+    writer = imageio.get_writer(output_file, fps=FPS, macro_block_size=None)
+
     algorithm_running = True
     clock = pygame.time.Clock()
     while True:
+        frame = pygame.surfarray.array3d(screen)
+        frame = np.flip(frame, axis=1)
+        frame = np.rot90(frame)
+        writer.append_data(frame)
+
+        pygame.display.update()
         clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -163,7 +205,8 @@ def path_finding(dir,alg):
 
 
 
-                        
+                    
+          
                     pygame.time.wait(70)
                     pygame.display.update()
            
